ea2551952c83a4f824e74377602f50509be18de3
Riskoder <g.vidal1g+1@gmail.com>
2024-07-27 23:24:08 -0400

    ejercicio 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+  if (num  == 0) return 0;
+  if (num < 0) return 'OOPS';
+
+  let prev = 1, current = 1;
+  for (let i = 3; i <= num; i++) {
+    let next = prev + current;
+    prev = current;
+    current = next;
+  };
+  return current;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// fb = [0, 1];
+// for (let i = 2; i <= num; i++) {
+//   fb[i] = fb[i - 1] + fb[i - 2];
