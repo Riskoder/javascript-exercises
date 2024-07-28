e60de5732b13dab2f07e9e70dcc0dd1a5491fed2
Riskoder <g.vidal1g+1@gmail.com>
2024-07-27 23:24:24 -0400

    ejercicio 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentDate = new Date().getFullYear()
 
+  return people.reduce((oldest, person) =>{
+    const personYearOfDeath = person.yearOfDeath || currentDate;
+    const personAge = personYearOfDeath - person.yearOfBirth;
+
+    const oldesYearOfDeath = oldest.yearOfDeath || currentDate;
+    const oldesAge = oldesYearOfDeath - oldest.yearOfBirth;
+
+    return personAge > oldesAge ? person : oldest
+  })
 };
 
 // Do not edit below this line
