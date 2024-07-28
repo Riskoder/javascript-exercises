4335f161bdd6a32ca76dca3e7661858e12fa8784
Riskoder <g.vidal1g+1@gmail.com>
2024-07-27 22:07:39 -0400

    quinto ejercicio

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(minimo, maximo) {
+  if (!Number.isInteger(minimo) || !Number.isInteger(maximo)) return 'ERROR';
+  if (minimo < 0) return 'ERROR';
 
+  if (minimo > maximo) {
+    let temp = minimo;
+    minimo = maximo;
+    maximo = temp;
+  }
+
+  let sumaTotal = 0;
+  for (let i = minimo; i <= maximo; i++) {
+    sumaTotal += i
+  };
+  return sumaTotal;
 };
 
 // Do not edit below this line
