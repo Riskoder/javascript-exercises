d868dab4a1e9c8d35639f04218ca6b65fa4d1fbf
Riskoder <g.vidal1g+1@gmail.com>
2024-07-27 23:23:51 -0400

    ejercicio N9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// let alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+// const cleanString = string.toLowerCase().replace(alphanumerical, '')
+
+// const reversedCleanString = cleanString.split('').reverse().join('')
